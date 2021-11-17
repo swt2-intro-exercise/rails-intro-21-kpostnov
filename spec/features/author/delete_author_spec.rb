1c4c6224732f0fdabf9a10060285e41c46ebccad
Kirill Postnov <kirill.postnov@student.hpi.uni-potsdam.de>
2021-11-17 15:20:03 +0100

    Implement issue #11

--- spec/features/author/delete_author_spec.rb
+++ spec/features/author/delete_author_spec.rb
@@ -7,14 +7,13 @@ describe "Deleting Authors", type: :feature do
     end
 
     it "should delete an author from the database" do
-        visit author_path(@author)
+        visit authors_path
 
         old_count = Author.count
-        
-        expect(page).to have_selector(:link_or_button, "Delete Author")
-        find_button("Delete Author").click
+
+        expect(page).to have_selector(:link_or_button, "Destroy")
+        find_link("Destroy").click
 
         expect(Author.count).to eq(old_count - 1)
-        
     end
 end
