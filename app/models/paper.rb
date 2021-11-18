5eb38c038c7d20975f1e9ca2c81d07c289622633
Kirill Postnov <kirill.postnov@student.hpi.uni-potsdam.de>
2021-11-18 16:22:16 +0100

    Implement issue #19

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -4,4 +4,8 @@ class Paper < ApplicationRecord
     validates :year, presence: true, numericality: { only_integer: true }
 
     has_and_belongs_to_many :authors
+
+    def self.year(num)
+        where("year == ?", num) if num.present?
+    end
 end
